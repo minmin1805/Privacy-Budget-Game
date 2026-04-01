a6e2a6862b201cce8a498e11ace409b2157906ef
minmin1805 <doanlyminh2005@gmail.com>
2026-04-01 02:59:29 -0400

    added mock images to GamePage

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
 
+import profileImage from '../assets/Photo/GamePage/mockprofile.png'
+import postImage from '../assets/Photo/GamePage/postimage.png'
+
+import { FaHeart } from "react-icons/fa";
+import { IoShield } from "react-icons/io5";
+
 export default function GamePage() {
   return (
     <div>
