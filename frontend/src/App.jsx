9641ad924919804beb78687fc25cfcbde51d7aa1
minmin1805 <doanlyminh2005@gmail.com>
2026-04-07 14:23:53 -0400

    integrate music and sound context to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,62 @@ import WelcomePage from './pages/WelcomePage'
 import InstructionPage from './pages/InstructionPage'
 import GamePage from './pages/GamePage'
 import EndgamePage from './pages/EndgamePage'
+import MusicToggleButton from './components/MusicToggleButton'
+import { SoundProvider, useSounds } from './context/SoundContext'
+import { MusicProvider, useMusic } from './context/MusicContext'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+function AppContent() {
+  const { playClickSound, playButtonClickSound } = useSounds()
+  const { startMusic } = useMusic()
+  const location = useLocation()
+
+  useEffect(() => {
+    const handleGlobalClick = (ev) => {
+      if (ev.target.closest('[data-skip-global-click-sound]')) return
+      if (ev.target.closest('button, [role="button"]')) {
+        playButtonClickSound()
+      } else {
+        playClickSound()
+      }
+    }
+    document.addEventListener('click', handleGlobalClick, true)
+    return () => document.removeEventListener('click', handleGlobalClick, true)
+  }, [playClickSound, playButtonClickSound])
+
+  // Attempt to start background music automatically (autoplay may still be blocked by the browser).
+  useEffect(() => {
+    if (location.pathname === '/') return
+    startMusic()
+  }, [location.pathname])
+
+  return (
+    <>
+      {location.pathname !== '/' && <MusicToggleButton />}
+      <Routes>
+        <Route path="/" element={<ContentWarningPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/instruction" element={<InstructionPage />} />
+        <Route path="/game" element={<GamePage />} />
+          <Route path="/endgame" element={<EndgamePage />} />
+      </Routes>
+    </>
+  )
+}
 
 function App() {
   return (
     <Router>
       <PrivacyBudgetProvider>
-        <Routes>
-          <Route path="/" element={<ContentWarningPage />} />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/instruction" element={<InstructionPage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/endgame" element={<EndgamePage />} />
-        </Routes>
-      </PrivacyBudgetProvider>
+      <SoundProvider>
+        <MusicProvider>
+          <Router>
+            <AppContent />
+          </Router>
+        </MusicProvider>
+      </SoundProvider>      
+    </PrivacyBudgetProvider>
     </Router>
   )
 }
